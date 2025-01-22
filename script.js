58c2eae6eb45f1b5b14093a5f0e6e0fe8bb20c40
sksksky <spbarte22@src.pshs.edu.ph>
2025-01-22 11:58:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const I = 3.14;
 document.write(I);
 document.write('<br><br>');
 let radius = 6.378e6; 
-document.write('<br><br>');
+document.write('The radius is ');
 document.write (radius);
 document.write('<br><br>');
-let C = 2*(I*radius);
+let circumference = 2*(I*radius);
 document.write('<br><br>');
-document.write(C)
-let A = 4*I*(radius**2);
-document.write('<br><br>');
-document.write(A);
+document.write(circumference)
+let surfaceArea= 4*I*(radius**2);
+document.write('The surface area is ');
+document.write(surfaceArea);
 document.write('<br><br>');
 let V = (4/3) * (I * r ** 3);
-document.write('<br><br>');
+document.write('The volume is ');
 document.write(V);
