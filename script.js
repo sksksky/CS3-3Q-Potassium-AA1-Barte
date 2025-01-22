77558e7f88e54572c59422ae1a9f058e98c4f272
sksksky <spbarte22@src.pshs.edu.ph>
2025-01-22 11:08:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const PI = 3.14;
 var radius;
-let circumference = 2 * PI * r;
-let surfaceArea = 4 * PI * r ** 2
-let volume = (4/3) * PI * r ** 3
+let circumference = 2 * PI * radius:
+let surfaceArea = 4 * PI * radius ** 2
+let volume = (4/3) * PI * radius ** 3
 document.write('The radius of the sphere is ' + radius + '.' + <br>);
 document.write('The circumference is ' + circumference + '.'+ <br>);
 document.write('The surface area is ' + surfaceArea + '.'+ <br>);
