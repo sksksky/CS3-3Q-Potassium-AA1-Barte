199e0823c8cba7b2c9b0cc1c350301b1090760c4
sksksky <spbarte22@src.pshs.edu.ph>
2025-01-22 11:51:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const P = 3.14;
 let radius = 6.378e6;
-let circumference = 2 * P * radius;
-let surfaceArea = 4 * P * radius ** 2;
-let volume = (4/3) * P * radius ** 3;
+let circumference = 2 *( P * radius);
+let surfaceArea = 4 * P * (radius ** 2);
+let volume = (4/3) * P * (radius ** 3);
 document.write('The radius of the sphere is ' + radius + '.' + <br>);
 document.write('The circumference is ' + circumference + '.'+ <br>);
 document.write('The surface area is ' + surfaceArea + '.'+ <br>);
