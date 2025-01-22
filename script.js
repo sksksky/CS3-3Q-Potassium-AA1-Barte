431aa8f78d16cf802904ff8a603d862f4d4c729e
sksksky <spbarte22@src.pshs.edu.ph>
2025-01-22 12:58:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ document.write('The radius is ');
 document.write (radius);
 document.write('<br><br>');
 let circumference = 2*(P*radius);
-document.write('<br><br>');
+document.write('The circumference is');
 document.write(circumference)
+document.write('<br><br>');
 let surfaceArea= 4*P*(radius**2);
 document.write('The surface area is ');
 document.write(surfaceArea);
