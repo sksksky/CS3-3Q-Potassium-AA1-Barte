a2ae32c460036b4ef2c09e4f6d344881fa620304
sksksky <spbarte22@src.pshs.edu.ph>
2025-01-22 11:55:07 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const I = 3.14;
+document.write(I);
+document.write('<br><br>');
+let radius = 6.378e6; 
+document.write('<br><br>');
+document.write (radius);
+document.write('<br><br>');
+let C = 2*(I*radius);
+document.write('<br><br>');
+document.write(C)
+let A = 4*I*(radius**2);
+document.write('<br><br>');
+document.write(A);
+document.write('<br><br>');
+let V = (4/3) * (I * r ** 3);
+document.write('<br><br>');
+document.write(V);
